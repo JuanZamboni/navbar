4622c26e81927e2ece6e15f3ff7d557d687d914a
JP <juanzamboni718@gmail.com>
2022-08-22 22:03:26 -0300

    ,

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+export const Navbar = () => {
+    return (
+          <header>
+          
+            <h1>Tech Store</h1>
+  
+            <nav>
+              <a>Home</a>
+              <a>Productos</a>
+              <a>Contacto</a>
+              <a>Nosotros</a>
+            </nav>
+           
+          </header>
+    )
+   }
